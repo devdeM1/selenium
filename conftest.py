36d99c0b7b26f345f875d4e56f9f7800d284f383
devdeM1 <makd1232255@yandex.ru>
2025-03-14 18:52:46 +0300

    Added testing in two languages

--- conftest.py
+++ conftest.py
@@ -3,10 +3,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-@pytest.fixture(scope="function")
-def driver():
+@pytest.fixture(scope="function", params=["en", "ru"])
+def driver(request):
+    lang = request.param
     chrome_options = Options()
-    chrome_options.add_argument("--lang=en")
+    chrome_options.add_argument(f"--lang={lang}")
     chrome_options.add_argument("--start-maximized")
     driver = webdriver.Chrome(options=chrome_options)
     driver.get('https://store.steampowered.com')
