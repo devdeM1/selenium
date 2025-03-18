b8938123a1ce0517978368d48f0ae78d2839ac9f
devdeM1 <makd1232255@yandex.ru>
2025-03-18 13:19:54 +0300

    Add test with JS alert

--- herokuapp/pages/js_alerst_page.py
+++ herokuapp/pages/js_alerst_page.py
@@ -4,8 +4,23 @@ from herokuapp.pages.base_page import BasePage
 
 
 class JavaScriptAlertsPage(BasePage):
+    TITLE_AFTER_CLOSE_ALERT = (By.XPATH, "//p[@id='result']")
     BUTTON_JS_ALERT = (By.XPATH, "//button[@onclick='jsAlert()']")
-    url = "https://theinternet.herokuapp.com/javascript_alerts"
+    BUTTON_JS_CONFIRM = (By.XPATH, "//button[@onclick='jsConfirm()']")
+    BUTTON_JS_PROMPT = (By.XPATH, "//button[@onclick='jsPrompt()']")
+    url = "https://the-internet.herokuapp.com/javascript_alerts"
 
     def open(self):
         self.driver.get(self.url)
+
+    def click_button_js_alert(self):
+        self.web_driver.get_present_element(self.driver, JavaScriptAlertsPage.BUTTON_JS_ALERT).click()
+
+    def click_button_js_confirm(self):
+        self.web_driver.get_present_element(self.driver, JavaScriptAlertsPage.BUTTON_JS_CONFIRM).click()
+
+    def click_button_js_prompt(self):
+        self.web_driver.get_present_element(self.driver, JavaScriptAlertsPage.BUTTON_JS_PROMPT).click()
+
+    def get_result_output(self):
+        return self.web_driver.get_present_element(self.driver, JavaScriptAlertsPage.TITLE_AFTER_CLOSE_ALERT).text
--- herokuapp/tests/basic_auth.py
+++ herokuapp/tests/basic_auth.py
@@ -1,3 +1,5 @@
+from faker import Faker
+
 from herokuapp.pages.basic_auth_page import BasicAuthPage
 from herokuapp.conftest import driver
 from herokuapp.pages.js_alerst_page import JavaScriptAlertsPage
@@ -9,6 +11,31 @@ def test_basic_auth(driver):
     auth_page.authorize('admin', 'admin')
     assert auth_page.is_auth_successful(), "Authorization failed."
 
+
 def test_javascript_alert(driver):
     js_alerts_page = JavaScriptAlertsPage(driver)
     js_alerts_page.open()
+    assert js_alerts_page.page_is_successfully_open(js_alerts_page.BUTTON_JS_ALERT), "The page is not opened correctly"
+    js_alerts_page.click_button_js_alert()
+    alert = driver.switch_to.alert
+    alert_text = alert.text
+    assert alert_text == "I am a JS Alert", "Unexpected alert text!"
+    alert.accept()
+    assert js_alerts_page.get_result_output() == "You successfully clicked an alert", \
+        "Something is wrong with the alert"
+    js_alerts_page.click_button_js_confirm()
+    alert = driver.switch_to.alert
+    alert_text = alert.text
+    assert alert_text == "I am a JS Confirm", "Unexpected alert text!"
+    alert.accept()
+    assert js_alerts_page.get_result_output() == "You clicked: Ok", "Something is wrong with the alert"
+    js_alerts_page.click_button_js_prompt()
+    alert = driver.switch_to.alert
+    alert_text = alert.text
+    assert alert_text == "I am a JS prompt", "Unexpected alert text!"
+    fake = Faker()
+    random_text = fake.sentence()
+    alert.send_keys(random_text)
+    alert.accept()
+    assert js_alerts_page.get_result_output() == f"You entered: {random_text}", \
+        "Prompt input was not processed correctly"
